e7655103ec9f0abf4afba31cb27f808286abe694
chichoch@gmail.com <chichoch@gmail.com@4cee5273-80dc-74ea-b2e9-b0162d060ce8>
2011-03-22 13:03:57 +0000

    TJENÄÄÄÄ

--- trunk/HelloWorld/src/HelloWorld.java
+++ trunk/HelloWorld/src/HelloWorld.java
@@ -1,6 +1,8 @@
+import javax.swing.*;
 
 public class HelloWorld {
 	public static void main (String[] arg) {
 		System.out.println("HELLO WORLD");
+		JOptionPane.showMessageDialog(null, "TJENÄÄÄÄÄÄÄ!");
 	}
 }
