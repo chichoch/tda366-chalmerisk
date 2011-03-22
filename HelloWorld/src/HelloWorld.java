f66087e93046144d70454b8c0f74ff5ec45dd143
chichoch <chichoch@gmail.com>
2011-03-22 13:12:39 +0000

    Ny class + frän grej

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -2,7 +2,8 @@ import javax.swing.*;
 
 public class HelloWorld {
 	public static void main (String[] arg) {
-		System.out.println("HELLO WORLD");
+		String s = Test.getString();
+		System.out.println("HELLO WORLD" + s);
 		//JOptionPane.showMessageDialog(null, "TJENÄÄÄÄÄÄÄ!");
 	}
 }
