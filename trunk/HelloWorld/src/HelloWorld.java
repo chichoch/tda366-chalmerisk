d7de4fefe757822655cde0ce5063ba08c9b6b836
chichoch@gmail.com <chichoch@gmail.com@4cee5273-80dc-74ea-b2e9-b0162d060ce8>
2011-03-22 13:07:46 +0000

    Test v1,01

--- trunk/HelloWorld/src/HelloWorld.java
+++ trunk/HelloWorld/src/HelloWorld.java
@@ -3,6 +3,6 @@ import javax.swing.*;
 public class HelloWorld {
 	public static void main (String[] arg) {
 		System.out.println("HELLO WORLD");
-		JOptionPane.showMessageDialog(null, "TJENÄÄÄÄÄÄÄ!");
+		//JOptionPane.showMessageDialog(null, "TJENÄÄÄÄÄÄÄ!");
 	}
 }
