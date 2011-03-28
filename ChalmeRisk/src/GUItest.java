3695afb12c048b31a6f116e5824203799a1be6ba
chichoch <chichoch@gmail.com>
2011-03-28 06:15:30 +0000

    A testClass for the GUI, Maximized window etc.

---
+++ ChalmeRisk/src/GUItest.java
@@ -0,0 +1,20 @@
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
+import javax.swing.*;
+
+
+
+
+public class GUItest extends JFrame {
+	
+	public GUItest() {
+		setLayout(new BorderLayout());
+		 ImageIcon icon = new ImageIcon("/home/christophe/Hämtningar/riskmap_liten.png");
+		 JLabel l = new JLabel(icon);
+		 add(l);
+		 setExtendedState(getExtendedState()|JFrame.MAXIMIZED_BOTH);
+		 setVisible(true);
+		 pack();
+	}
+}
--- ChalmeRisk/src/Main.java
+++ ChalmeRisk/src/Main.java
@@ -2,5 +2,6 @@
 public class Main {
 	public static void main (String[] arg) {
 		System.out.println("ChalmeRisk!");
+		new GUItest();
 	}
 }
