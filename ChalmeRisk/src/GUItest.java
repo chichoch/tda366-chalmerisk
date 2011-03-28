6253174b88bb69b15e99c809f74a254d6612c559
chichoch <chichoch@gmail.com>
2011-03-28 08:30:19 +0000

    Title set to "ChalmeRisk"

--- ChalmeRisk/src/GUItest.java
+++ ChalmeRisk/src/GUItest.java
@@ -7,11 +7,11 @@ import javax.swing.*;
 
 
 public class GUItest extends JFrame {
-	
 	public GUItest() {
 		setLayout(new BorderLayout());
 		ImageIcon icon = new ImageIcon("/home/christophe/Hämtningar/riskmap_liten.png");
 		JLabel l = new JLabel(icon);
+		setTitle("ChalmeRisk");
 		add(l);
 		setExtendedState(getExtendedState()|JFrame.MAXIMIZED_BOTH);
 		setVisible(true);
