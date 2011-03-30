9aedf537803ee07ba4d228eabafc102575032180
f.ehrndal <f.ehrndal@gmail.com>
2011-03-30 09:35:13 +0000

    annan karta

--- ChalmeRisk/src/GUItest.java
+++ ChalmeRisk/src/GUItest.java
@@ -1,10 +1,16 @@
 import java.awt.BorderLayout;
 import javax.swing.*;
 
+
 public class GUItest extends JFrame {
+	private JButton leftUpperCountry;
+	private JButton rightUpperCountry;
+	private JButton leftDownCountry;
+	private JButton rightDownCountry;
+	
 	public GUItest() {
 		setLayout(new BorderLayout());
-		ImageIcon icon = new ImageIcon("/home/christophe/Hämtningar/riskmap_liten.png");
+		ImageIcon icon = new ImageIcon("Karta.jpg");
 		JLabel l = new JLabel(icon);
 		setTitle("ChalmeRisk");
 		add(l);
