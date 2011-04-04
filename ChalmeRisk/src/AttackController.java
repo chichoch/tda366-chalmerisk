417dce6002f53b2b1b1e4c9ee3f8d57a617a97ab
chichoch <chichoch@gmail.com>
2011-04-04 14:37:55 +0000

    AttackController changes

--- ChalmeRisk/src/AttackController.java
+++ ChalmeRisk/src/AttackController.java
@@ -1,17 +1,28 @@
 
 public class AttackController {
+	
 	private Country c1;
 	private Country c2;
+	private Map map;
 	private boolean firstCountrySelected = false;
+	
+	//Constructor
+	public AttackController() {
+		
+	}
 	
 	public void setCountry(int id) {
 		if (firstCountrySelected) {
-			//c2 = new Country(Map.getCountry());
+			c2 = map.getCountry(id);
+			new AttackDialog(c1, c2);
+		}
+		else {
+			c1 = map.getCountry(id);
 		}
 	}
 	
 	public void startFight() {
-		
+	
 	}
 	
 	public void endFight() {
