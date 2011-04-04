a22c0b9a6b3593c35f694dedb329511650bfd408
chichoch@gmail.com <chichoch@gmail.com@4cee5273-80dc-74ea-b2e9-b0162d060ce8>
2011-04-04 20:11:42 +0000

    Attack is now possible, but use of Attack instead of AttackDialog for now.

--- trunk/ChalmeRisk/src/Map.java
+++ trunk/ChalmeRisk/src/Map.java
@@ -1,8 +1,14 @@
+import javax.swing.JOptionPane;
+
 
 public class Map {
 	
-	private Country Chalmers = new Country("Chalmers", 1, 10);
-	private Country Chalmers2 = new Country("Chalmers", 2, 10);
+	private Country Norge = new Country("Norge", 1, 10);
+	private Country Sverige = new Country("Sverige", 2, 10);
+	private Country Finland = new Country("Finland", 3, 10);
+	private Country Danmark = new Country("Danmark", 4, 10);
+	//A Country that indicates that something is wrong.
+	private Country empty = new Country("EMPTY COUNTRY", "ERROR", 3);
 	//private Country Chalmers = new Country("Chalmers");
 	//private Country Chalmers = new Country("Chalmers");
 	//private Country Chalmers = new Country("Chalmers");
@@ -10,13 +16,19 @@ public class Map {
 	
 	//Returns the specific ID of each country
 	public Country getCountry(int i){
-		if(Chalmers.getCountryId()== i){ 
-			return Chalmers;
+		if(Norge.getCountryId() == i){ 
+			return Norge;
 		}
-		if(Chalmers2.getCountryId()== i){ 
-			return Chalmers2;
+		if(Sverige.getCountryId() == i){ 
+			return Sverige;
 		}
-		return null;
+		if(Finland.getCountryId()== i){ 
+			return Finland;
+		}
+		if(Danmark.getCountryId()== i){ 
+			return Danmark;
+		}
+		return empty;
 	}
 	 
 }
