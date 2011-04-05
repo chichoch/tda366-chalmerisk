4892c8be6e54621d5bcecd418c767406dcd8fccb
joakim.b@gmail.com <joakim.b@gmail.com@4cee5273-80dc-74ea-b2e9-b0162d060ce8>
2011-04-05 13:51:35 +0000

    Removed enums

--- trunk/ChalmeRisk/src/Player.java
+++ trunk/ChalmeRisk/src/Player.java
@@ -1,20 +1,22 @@
+import java.awt.Color;
+
 
 public class Player {
 	
-	public enum Color{ BLUE, RED, GREEN, YELLOW, BLACK, BROWN}
+	private Color color;
 	private String name;
 	
-	private final Color color;
     private Player(Color color,String name) {
     	this.name = name;
         this.color = color;
     }
     
-    public Color color() {
+    public Color getColor() {
     	return color;
     }
     
     public String getName() {
     	return name;
     }
+    
 }
