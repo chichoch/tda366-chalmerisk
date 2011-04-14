ad10731ab4d8af0ac172ae0aa5183a697c6c1716
f.ehrndal <f.ehrndal@gmail.com>
2011-04-14 08:33:29 +0000

    Bugfix

--- ChalmeRisk/src/edu/chl/chalmerisk/risk/view/CountryView.java
+++ ChalmeRisk/src/edu/chl/chalmerisk/risk/view/CountryView.java
@@ -10,7 +10,6 @@ import javax.swing.*;
 import edu.chl.chalmerisk.risk.constants.Country;
 import edu.chl.chalmerisk.risk.core.Builder;
 
-
 public class CountryView extends JPanel implements Observer{
 	private JLabel troopDisplay;
 	private ImageIcon icon;
@@ -19,25 +18,25 @@ public class CountryView extends JPanel implements Observer{
 	
 	public CountryView(Country country){
 		this.country = country;
+		country.addObserver(this);
 		setLayout(new FlowLayout());
 		icon = Builder.iconHandler.getIcon(country.getOwner(), country.getTroops());
 		troopIcon = new JLabel();
-		troopDisplay = new JLabel("1");
+		troopDisplay = new JLabel(""+country.getTroops());
 		troopIcon.setIcon(icon);
 		add(troopIcon);
 		add(troopDisplay);
-		setPreferredSize(new Dimension(60,75));
+		setPreferredSize(new Dimension(75,75));
 		setOpaque(false);
 		country.addObserver(this);
-		
-	}
+		}
 	
 	@Override
 	public void update(Observable observable, Object arg) {
 		if(observable.equals(country)){
-			System.out.println("Ja");
 			if(arg.equals(new Integer(0))){
 				troopDisplay.setText(""+country.getTroops());
+				troopIcon.setIcon(Builder.iconHandler.getIcon(country.getOwner(), country.getTroops()));
 			}
 			if(arg.equals(new Integer(1))){
 				troopIcon.setIcon(Builder.iconHandler.getIcon(country.getOwner(), country.getTroops()));
