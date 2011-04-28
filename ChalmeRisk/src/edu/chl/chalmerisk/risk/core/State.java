417565d41d0c726feebbb05bf01986e188da43dd
radway.alexander <radway.alexander@gmail.com>
2011-04-28 11:44:34 +0000

    testing of state pattern for attackcontroller

---
+++ ChalmeRisk/src/edu/chl/chalmerisk/risk/core/State.java
@@ -0,0 +1,15 @@
+package edu.chl.chalmerisk.risk.core;
+
+abstract class State {
+
+	public void reinforcement(Turn turn){
+		
+	}
+	
+	public void attack(Turn turn){
+		
+	}
+	public void troopMovement(Turn turn){
+		
+	}
+}
