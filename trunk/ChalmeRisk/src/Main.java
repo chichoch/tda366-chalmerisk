ba265d629fa5ebe203c64fdb3ab2220790e1f21c
chichoch@gmail.com <chichoch@gmail.com@4cee5273-80dc-74ea-b2e9-b0162d060ce8>
2011-04-28 16:26:48 +0000

    Have some code for later testing in its comments.

--- trunk/ChalmeRisk/src/Main.java
+++ trunk/ChalmeRisk/src/Main.java
@@ -1,10 +1,40 @@
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.chl.chalmerisk.risk.constants.Player;
 import edu.chl.chalmerisk.risk.core.Builder;
+import edu.chl.chalmerisk.risk.core.Round;
 
 public class Main {
+	
 	public static void main (String[] arg) {
 		new Builder();
 		//new PaintDice();
 		
+		//The following code was just for testing:
+		/*
+		List<Player> list = new ArrayList<Player>();
+		Player p1 = new Player(Color.red, "Player1");
+		Player p2 = new Player(Color.BLACK, "Player2");
+		Player p3 = new Player(Color.red, "Player 3");
+		Player p4 = new Player(Color.red, "Player 4");
+		list.add(p1);
+		list.add(p2);
+		list.add(p3);
+		list.add(p4);
+		Round round = new Round(list);
+		for (int i = 0; i < 10; i++) {
+			round.newRound();
+		}
+		System.out.println("Number of players: " + round.getNumberOfPlayers());
 		
+		//Remove one player and test again:
+		list.remove(2);
+		for (int i = 0; i < 10; i++) {
+			round.newRound();
+		}
+		System.out.println("Number of players: " + round.getNumberOfPlayers());
+		*/
 	}
 }
