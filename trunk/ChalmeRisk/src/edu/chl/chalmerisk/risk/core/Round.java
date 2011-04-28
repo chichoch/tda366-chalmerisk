06fca5ec7da705b3267ca109f31aeca507070e1d
chichoch@gmail.com <chichoch@gmail.com@4cee5273-80dc-74ea-b2e9-b0162d060ce8>
2011-04-28 16:25:12 +0000

    Round-class created, and have getters for active players, List of players and number of players.

---
+++ trunk/ChalmeRisk/src/edu/chl/chalmerisk/risk/core/Round.java
@@ -0,0 +1,42 @@
+package edu.chl.chalmerisk.risk.core;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.chl.chalmerisk.risk.constants.Player;
+
+public class Round {
+	private List<Player> pList = new ArrayList<Player>();
+	private Player currentPlayer;
+	private int numOfPlayers;
+	
+	public Round(List<Player> list) {
+		pList = list;
+		currentPlayer = pList.get(0);
+		numOfPlayers = pList.size();
+	}
+	
+	public void newRound() {
+		try {
+			System.out.println("" + pList.indexOf(currentPlayer));
+			currentPlayer = pList.get(pList.indexOf(currentPlayer) + 1);
+			numOfPlayers = pList.size();
+		}
+		catch (IndexOutOfBoundsException e){
+			numOfPlayers = pList.size();
+			currentPlayer = pList.get(0);
+		}
+		
+	}
+	public int getNumberOfPlayers() {
+		return numOfPlayers;
+	}
+	
+	public Player getCurrentPlayer() {
+		return currentPlayer;
+	}
+	
+	public List<Player> getPlayerList() {
+		return pList;
+	}
+}
