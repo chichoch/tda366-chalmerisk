98a0db0a17e12cfaaa50c00bb3536649e15de260
chichoch@gmail.com <chichoch@gmail.com@4cee5273-80dc-74ea-b2e9-b0162d060ce8>
2011-04-28 16:29:12 +0000

    Started on the FileReader in package io.

--- trunk/ChalmeRisk/src/edu/chl/chalmerisk/risk/io/FileReader.java
+++ trunk/ChalmeRisk/src/edu/chl/chalmerisk/risk/io/FileReader.java
@@ -1,5 +1,15 @@
 package edu.chl.chalmerisk.risk.io;
 
-public class FileReader {
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 
+public class FileReader {
+	private List<String> sList = new ArrayList();
+	public List<String> getFile() throws FileNotFoundException {
+		sList.clear();
+		
+		
+		return sList;
+	}
 }
