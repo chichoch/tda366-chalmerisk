6df01a52dd36f9e506a83725861fa4ab3d5cd18f
radway.alexander@gmail.com <radway.alexander@gmail.com@4cee5273-80dc-74ea-b2e9-b0162d060ce8>
2011-05-02 14:30:46 +0000

    Updated movementdialog and troopmovementcontrol

--- trunk/ChalmeRisk/src/edu/chl/chalmerisk/risk/ctrl/TroopMovementController.java
+++ trunk/ChalmeRisk/src/edu/chl/chalmerisk/risk/ctrl/TroopMovementController.java
@@ -20,10 +20,13 @@ public class TroopMovementController extends TurnState {
 			if(Builder.map.getCountry(id).getTroops()<=1){
 				JOptionPane.showMessageDialog(null, "För få trupper för att förflytta!");
 			}
-			else{
+			else if(Builder.map.getCountry(id).getOwner().equals(Builder.round.getCurrentPlayer())){
 				firstCountrySelected = true;
 				firstSelectedCountry = Builder.map.getCountry(id);
 				JOptionPane.showMessageDialog(null, "Första landet markerat (" + Builder.map.getCountry(id).getName() + ")" );	
+			}
+			else{
+				JOptionPane.showMessageDialog(null, "Du måste förflytta trupper från ditt egna land" );
 			}
 		}
 		else {
@@ -32,20 +35,22 @@ public class TroopMovementController extends TurnState {
 				if (firstSelectedCountry.hasNeighbour(id) == true) {
 					JOptionPane.showMessageDialog(null, "Andra landet markerat (" + Builder.map.getCountry(id).getName() + ")" );
 					firstCountrySelected = false;
-					Builder.movement.newMovement();
-					//Builder.attack.newAttack(firstSelectedCountry, secondSelectedCountry);
-					//Builder.attack.repaintTroops(firstSelectedCountry.getTroops(), secondSelectedCountry.getTroops());
-					//Builder.attack.setVisible(true);
+					Builder.movement.newMovement(firstSelectedCountry, secondSelectedCountry);
+					Builder.movement.setVisible(true);
 				}
 				else {
 					JOptionPane.showMessageDialog(null, "Landet du vill förflytta trupper till måste vara ett grannland!");
 				}
 			}
 			else{
-				JOptionPane.showMessageDialog(null, "Du kan bara förflytta till dina egna länder");
+				JOptionPane.showMessageDialog(null, "Du kan bara förflytta mellan dina egna länder");
 				firstSelectedCountry = null;
 				firstCountrySelected = false;
 				}
 		}
+	}
+	public void doMovement(int value){
+		firstSelectedCountry.setTroops(firstSelectedCountry.getTroops()-value);
+		secondSelectedCountry.setTroops(secondSelectedCountry.getTroops()+value);
 	}
 }
