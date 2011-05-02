e5502e59d5e993ef8a5f2773648f629c5b283318
chichoch@gmail.com <chichoch@gmail.com@4cee5273-80dc-74ea-b2e9-b0162d060ce8>
2011-05-02 14:20:26 +0000

    FileReader and IFileReader created.

--- trunk/ChalmeRisk/src/edu/chl/chalmerisk/risk/io/FileReader.java
+++ trunk/ChalmeRisk/src/edu/chl/chalmerisk/risk/io/FileReader.java
@@ -1,15 +1,41 @@
 package edu.chl.chalmerisk.risk.io;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
-public class FileReader {
+public class FileReader implements IFileReader {
 	private List<String> sList = new ArrayList();
-	public List<String> getFile() throws FileNotFoundException {
+	public List<String> getFile(String filename) throws FileNotFoundException {
 		sList.clear();
+		Scanner sc = new Scanner(new FileInputStream(filename));
 		
-		
+		try {
+			while (sc.hasNextLine()) {
+				String s = sc.nextLine().trim();
+				if (s.length() == 0) {
+					// Skip empty lines
+				} else if (s.matches("#.*")) {
+					// Skip comments in file
+				} else {
+					sList.add(s);
+				}
+			}
+		} finally {
+			sc.close();
+		}
 		return sList;
 	}
+	
+	private FileReader() {
+		
+	}
+	
+	public static IFileReader getInstance() {
+		return new FileReader();
+	}
+	
 }
