8805242d9c6d50d6aa13c1efa2500d5524fb989d
chichoch@gmail.com <chichoch@gmail.com@4cee5273-80dc-74ea-b2e9-b0162d060ce8>
2011-05-05 06:53:47 +0000

    Subsystem almost completed and implemented!

--- trunk/ChalmeRisk/src/edu/chl/chalmerisk/risk/core/Builder.java
+++ trunk/ChalmeRisk/src/edu/chl/chalmerisk/risk/core/Builder.java
@@ -1,12 +1,14 @@
 package edu.chl.chalmerisk.risk.core;
 
 import java.awt.Color;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
 import edu.chl.chalmerisk.risk.constants.Map;
 import edu.chl.chalmerisk.risk.constants.Player;
 import edu.chl.chalmerisk.risk.ctrl.*;
+import edu.chl.chalmerisk.risk.io.MapBuilder;
 import edu.chl.chalmerisk.risk.view.*;
 
 
@@ -15,9 +17,9 @@ public class Builder {
 	public static Player player1 = new Player(Color.RED, "Fredrik");
 	public static Player player2 = new Player(Color.GREEN, "Christope");
 	private static List<Player> pList = new ArrayList<Player>();
-	public static Map map = new Map();
-	public static IconHandler iconHandler = new IconHandler();
-	public static GUItest guiTest = new GUItest();
+	public static Map map;
+	public static IconHandler iconHandler;
+	public static GUItest guiTest;
 	public static AttackDialog attack = new AttackDialog();
 	public static MovementDialog movement = new MovementDialog();
 	public static AttackController aCtrl = new AttackController();
@@ -26,10 +28,23 @@ public class Builder {
 	public static DiceController diceC = new DiceController();
 	public static Turn turn = new Turn();
 	public static Round round;
-	public Builder(){
+	public Builder() throws FileNotFoundException{
+		map = new Map("maps/testmap.txt");
+		setPlayers();
+		iconHandler  = new IconHandler();
+		guiTest = new GUItest();
+		
 		pList.add(player1);
 		pList.add(player2);
 		round = new Round(pList);
+	}
+	
+	//A preliminary method to set the players in the game.
+	public void setPlayers() {
+		map.setPlayer(player1, 1);
+		map.setPlayer(player1, 2);
+		map.setPlayer(player2, 3);
+		map.setPlayer(player2, 4);
 	}
 }
 
--- trunk/ChalmeRisk/src/edu/chl/chalmerisk/risk/io/MapBuilder.java
+++ trunk/ChalmeRisk/src/edu/chl/chalmerisk/risk/io/MapBuilder.java
@@ -1,11 +1,13 @@
 package edu.chl.chalmerisk.risk.io;
 
+import java.awt.Color;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 import edu.chl.chalmerisk.risk.constants.Country;
+import edu.chl.chalmerisk.risk.constants.Player;
 
 public class MapBuilder {
 	private IFileReader file;
@@ -20,6 +22,7 @@ public class MapBuilder {
 	/**
 	 * To do a map, you should write the information about the map like this:
 	 * Use # to comment in a file. 
+	 * Put the Map-file in "maps/*.txt"
 	 * 
 	 * First row should be the filename of the maps Iconfile.
 	 * After that you could start to define your countries like this:
@@ -60,13 +63,12 @@ public class MapBuilder {
 	 * 
 	 *
 	 * @throws FileNotFoundException
+	 * @author Christophe
 	 */
 	
- 	public MapBuilder() throws FileNotFoundException {
- 		//Fulkod för våran testfil.
- 		String fileName = "maps/testmap.txt";
- 		
-		file = FileReader.getInstance();
+ 	public MapBuilder(String fileName) throws FileNotFoundException { 		
+		
+ 		file = FileReader.getInstance();
 		List<String> command = file.getFile(fileName);
 		numOfCountries = (command.size() - 1) /5;
 		mapIconFileName = command.get(0);
@@ -80,13 +82,43 @@ public class MapBuilder {
 			countryY = Integer.parseInt(command.get((i * 5) + 4));
 			
 			Scanner sc = new Scanner(command.get((i * 5) + 5));
+			int numOfNeighbours = 0;
 			while (sc.hasNextInt()) {
-			
+				numOfNeighbours++;
+				sc.nextInt();
 			}
-			
+			countryNeighboors = new int[numOfNeighbours];
+			sc.close();
+			Scanner sca = new Scanner(command.get((i * 5) + 5));
+			for (int j = 0; j < countryNeighboors.length; j++) {
+				countryNeighboors[j] = sca.nextInt();
+				
+			}
+			Player p = new Player(Color.black, "Random PLayer");
+			//Creates a new Country with a standard number of troops (1), and a "Random Player"
+			Country c = new Country(countryName, countryId, countryX, countryY, 1, countryNeighboors, p);
+			countries.add(c);
 		}
-		System.out.println("Antal länder:" + numOfCountries + "\n" + command.get(0) + "\n " + command.get(1) + "\n " + command.get(2) 
-				+ "\n " + command.get(3) + "\n " + command.get(4) + "\n " + command.get(5));
+		System.out.println("Antal l�nder: " + numOfCountries);
 	}
+ 	
+ 	public List<Country> getCountries() {
+ 		return countries;
+ 	}
+ 	
+ 	public Country getCountry(int id) {
+ 		for (int i = 0; i < countries.size(); i++) {
+ 			if (countries.get(i).getCountryId() == id) {
+ 				return countries.get(i);
+ 			}
+ 		}
+ 		return null;
+ 	}
+ 	
+ 	public String getIconFileName() {
+ 		return mapIconFileName;
+ 	}
+ 	
+ 	
 	
 }
