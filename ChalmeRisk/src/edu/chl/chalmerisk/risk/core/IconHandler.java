71196d794bc53ba46e1b22f067d4dd51c9151bcc
f.ehrndal <f.ehrndal@gmail.com>
2011-05-10 13:37:28 +0000

    All color for all 4 players are now implemented

--- ChalmeRisk/src/edu/chl/chalmerisk/risk/core/IconHandler.java
+++ ChalmeRisk/src/edu/chl/chalmerisk/risk/core/IconHandler.java
@@ -6,11 +6,17 @@ import edu.chl.chalmerisk.risk.constants.Player;
 
 public class IconHandler {
 	ImageIcon redKnight = new ImageIcon("resources/KnightRed.gif");
+	ImageIcon blueKnight = new ImageIcon("resources/KnightBlue.gif");
+	ImageIcon yellowKnight = new ImageIcon("resources/KnightYellow.gif");
+	ImageIcon greenKnight = new ImageIcon("resources/KnightGreen.gif");
 	ImageIcon greenInfantrie = new ImageIcon("resources/greenInfantry.gif");
 	ImageIcon redInfantrie = new ImageIcon("resources/redInfantry.gif");
+	ImageIcon blueInfantrie = new ImageIcon("resources/blueInfantry.gif");
+	ImageIcon yellowInfantrie = new ImageIcon("resources/yellowInfantry.gif");
 	ImageIcon greenCannon = new ImageIcon("resources/greenCannon.gif");
 	ImageIcon redCannon = new ImageIcon("resources/redCannon.gif");
-	ImageIcon greenKnight = new ImageIcon("resources/KnightSmall.gif");
+	ImageIcon yellowCannon = new ImageIcon("resources/yellowCannon.gif");
+	ImageIcon blueCannon = new ImageIcon("resources/blueCannon.gif");
 	ImageIcon compInf = new ImageIcon("resources/greyInfantry.gif");
 	
 	public IconHandler(){
@@ -25,6 +31,12 @@ public class IconHandler {
 			else if(p.getColor()==Color.GREEN){
 				return greenInfantrie;
 			}
+			else if(p.getColor()==Color.YELLOW){
+				return yellowInfantrie;
+			}
+			else if(p.getColor()==Color.BLUE){
+				return blueInfantrie;
+			}
 		}
 		else if(i>=5 && i<10){
 			if(p.getColor()==Color.RED){
@@ -33,6 +45,12 @@ public class IconHandler {
 			else if(p.getColor()==Color.GREEN){
 				return greenKnight;
 			}
+			else if(p.getColor()==Color.YELLOW){
+				return yellowKnight;
+			}
+			else if(p.getColor()==Color.BLUE){
+				return blueKnight;
+			}
 		}
 		else{
 			if(p.getColor()==Color.RED){
@@ -40,6 +58,12 @@ public class IconHandler {
 			}
 			else if(p.getColor()==Color.GREEN){
 				return greenCannon;
+			}
+			else if(p.getColor()==Color.YELLOW){
+				return yellowCannon;
+			}
+			else if(p.getColor()==Color.BLUE){
+				return blueCannon;
 			}
 		}
 		return null;
