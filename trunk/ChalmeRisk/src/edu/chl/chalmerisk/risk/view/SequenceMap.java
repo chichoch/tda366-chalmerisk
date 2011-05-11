84f36dbf598ef54e0c97f596dad65e2f85a17868
joakim.b@gmail.com <joakim.b@gmail.com@4cee5273-80dc-74ea-b2e9-b0162d060ce8>
2011-05-11 10:38:50 +0000

    Work on the SequenceMap

--- trunk/ChalmeRisk/src/edu/chl/chalmerisk/risk/view/SequenceMap.java
+++ trunk/ChalmeRisk/src/edu/chl/chalmerisk/risk/view/SequenceMap.java
@@ -1,7 +1,31 @@
 package edu.chl.chalmerisk.risk.view;
+import java.util.Observable;
+import java.util.Observer;
+
+import javax.swing.Icon;
 import javax.swing.JLabel;
 
-public class SequenceMap extends JLabel{
+import edu.chl.chalmerisk.risk.core.ChalmeRisk;
+
+public class SequenceMap extends JLabel implements Observer {
+	private Icon imageIcon;
+	public SequenceMap() {
+		ChalmeRisk.turn.addObserver(this);
+	}
+	@Override
+	public void update(Observable observable, Object arg) {
+		// TODO Auto-generated method stub
+		if(observable.equals(ChalmeRisk.turn)){
+			if(arg.equals(new Integer(0))) {
+				setText("" + ChalmeRisk.turn.getCurrentStateIndex());
+			}
+			if(arg.equals(new Integer(1))) {
+				
+			}
+		}
+		setIcon(imageIcon);
+		
+	}
 	
 	
 }
