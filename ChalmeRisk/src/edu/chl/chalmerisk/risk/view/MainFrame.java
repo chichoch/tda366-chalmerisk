ee139c6d4622498f11fcf8e89bb4d1148a73b065
chichoch <chichoch@gmail.com>
2011-05-12 14:25:04 +0000

    Implemented a ReinforcementPanel in mainframe, which shows how many reinforcements that you have left.

--- ChalmeRisk/src/edu/chl/chalmerisk/risk/view/MainFrame.java
+++ ChalmeRisk/src/edu/chl/chalmerisk/risk/view/MainFrame.java
@@ -10,6 +10,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.util.List;
 
 import javax.swing.*;
@@ -18,7 +19,7 @@ import edu.chl.chalmerisk.risk.constants.Country;
 import edu.chl.chalmerisk.risk.core.ChalmeRisk;
 
 
-public class MainFrame extends JFrame implements MouseListener, ActionListener {
+public class MainFrame extends JFrame implements MouseListener, ActionListener, MouseMotionListener {
 	private JLayeredPane karta;
 	private JPanel bottom;
 	private JPanel top;
@@ -27,15 +28,21 @@ public class MainFrame extends JFrame implements MouseListener, ActionListener {
 	private PlayerView pv;
 	private SequenceMap sm;
 	private InfoView infoView;
+	private ReinforcementPanel reinforcementPanel;
 	
 	//TODO No hardcode here!
 	public MainFrame(List<Country> list) {
 		setLayout(new BorderLayout());
 		ImageIcon icon = new ImageIcon(ChalmeRisk.map.getIconFileName());
-		
+		reinforcementPanel = new ReinforcementPanel(ChalmeRisk.round.getCurrentPlayer().getReinforcements());
+		addMouseMotionListener(this);
+		reinforcementPanel.addMouseMotionListener(this);
+
+		reinforcementPanel.setBounds(140, 140, 30, 30);
+		reinforcementPanel.setBorder(BorderFactory.createEmptyBorder(1, 1, 1, 1));
 		karta = new JLayeredPane();
 		karta.setBackground(Color.BLACK);
-		
+		karta.add(reinforcementPanel, JLayeredPane.DEFAULT_LAYER);
 		//Set icons
 		JLabel l = new JLabel(icon);
 		for (int i = 0; i < list.size(); i++) {
@@ -44,11 +51,13 @@ public class MainFrame extends JFrame implements MouseListener, ActionListener {
 			cv.setBorder(BorderFactory.createEmptyBorder(1, 1, 1, 1));
 			karta.add(cv, JLayeredPane.DEFAULT_LAYER);
 			cv.addMouseListener(this);
+			cv.addMouseMotionListener(this);
 		}
 		
         l.setIcon(icon); // NOI18N
         l.setBounds(-18, -80, 1400, 800);
         karta.add(l, JLayeredPane.DEFAULT_LAYER);
+        
 	
 		bottom = new JPanel();
 		bottom.setLayout(new BorderLayout());
@@ -106,7 +115,7 @@ public class MainFrame extends JFrame implements MouseListener, ActionListener {
 
 	@Override
 	public void mouseEntered(MouseEvent arg0) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
@@ -132,6 +141,24 @@ public class MainFrame extends JFrame implements MouseListener, ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		if(e.getSource()== nextStep){
 			ChalmeRisk.turn.changeState();
+		}
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	//TODO Maybe not the best way to check if we should show the reinforcementPanel?
+	@Override
+	public void mouseMoved(MouseEvent arg0) {
+		if (ChalmeRisk.turn.getCurrentStateIndex() == 0) {
+			reinforcementPanel.setVisible(true);
+			reinforcementPanel.setLocation((int)getMousePosition().getX() , (int)getMousePosition().getY() - 50);
+			validate();
+		}
+		else {
+			reinforcementPanel.setVisible(false);
 		}
 	}
 }
