561e9bd2b77c4e51beac17983286757a0deb0a6d
joakim.b@gmail.com <joakim.b@gmail.com@4cee5273-80dc-74ea-b2e9-b0162d060ce8>
2011-05-12 13:03:42 +0000

    Completly changed ActivePlayers

--- trunk/ChalmeRisk/src/edu/chl/chalmerisk/risk/util/ActivePlayers.java
+++ trunk/ChalmeRisk/src/edu/chl/chalmerisk/risk/util/ActivePlayers.java
@@ -1,37 +1,37 @@
 package edu.chl.chalmerisk.risk.util;
 import java.util.ArrayList;
 import java.util.List;
-
+ 
 import edu.chl.chalmerisk.risk.constants.*;
-
+import edu.chl.chalmerisk.risk.core.ChalmeRisk;
+ 
 public class ActivePlayers {
-	private static ActivePlayers instance;
-	
-	private ActivePlayers() {
-		
-	}
-	//TODO This method is maybe responsible for a "java.lang.OutOfMemoryError: Java heap space" thing.
-	public List<Player> getActivePlayers(List<Country> cList) {
-		List<Player> pList = new ArrayList<Player>();
-		pList.add(cList.get(0).getOwner());
-		for (int i = 0; i < cList.size(); i++) {
-			for (int j = 0; j < pList.size(); j++) {
-				if (!cList.get(i).getOwner().equals(pList.get(j))) {
-					pList.add(cList.get(i).getOwner());
-				}
-			}
-		}
-		if (pList.size() == 1) {
-			//TODO Should throw a GameOverException() ?
-			//throw new GameOverException();
-		}
-		return pList;
-	}
-	
-	public static synchronized ActivePlayers getInstance() {
-		if(instance == null){
-			instance = new ActivePlayers();
-		}
-		return instance;
-	}
+    private static ActivePlayers instance;
+    private int count;
+    private ActivePlayers() {
+   	 
+    }
+    //TODO This method is maybe responsible for a "java.lang.OutOfMemoryError: Java heap space" thing.
+    public boolean getActivePlayers(Player player) {
+   	 count = 0;
+   	 List<Country> cList = new ArrayList<Country>();
+   	 cList = ChalmeRisk.map.getCountries();
+   	 for (int i = 0; i < cList.size(); i++) {
+   		 if(cList.get(i).getOwner().equals(player)){
+   			 count++;
+   		 }
+   	 }
+   	 if(count == 0){
+   		 return false;
+   		 
+   	 }
+   	 return true;
+    }
+ 
+    public static synchronized ActivePlayers getInstance() {
+   	 if(instance == null){
+   		 instance = new ActivePlayers();
+   	 }
+   	 return instance;
+    }
 }
