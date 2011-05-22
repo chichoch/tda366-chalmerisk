eaa501ea121ac28e70d587ceb94075b082e3eac9
chichoch@gmail.com <chichoch@gmail.com@4cee5273-80dc-74ea-b2e9-b0162d060ce8>
2011-05-22 08:33:11 +0000

    More tests done.

--- trunk/ChalmeRisk/test/edu/chl/chalmerisk/risk/ReinforcementTest.java
+++
@@ -1,5 +0,0 @@
-package edu.chl.chalmerisk.risk;
-
-public class ReinforcementTest {
-
-}
---
+++ trunk/ChalmeRisk/test/edu/chl/chalmerisk/risk/io/FileReaderTest.java
@@ -0,0 +1,48 @@
+package edu.chl.chalmerisk.risk.io;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class FileReaderTest {
+	
+	private IFileReader filereader;
+	
+	@Before
+	public void setUp() {
+		filereader = FileReader.getInstance();
+	}
+	
+	/**Test if we can get a List of Strings, and also if we can comment in the files.
+	 * We also checks if we can use empty lines.
+	 *	
+	 * The testfile looks like this:
+	 * _________________________________
+	 * 	test1
+		#Commented line.
+		
+		test2
+		test3
+		test4
+	 */
+	@Test
+	public void FileReaderTest() throws FileNotFoundException {
+		List<String> list = new ArrayList<String>();
+		list = filereader.getFile("test/test.txt");
+		
+		Assert.assertTrue(list.size() == 4);
+		Assert.assertTrue(list.get(0).equals("test1"));
+		Assert.assertTrue(list.get(1).equals("test2"));
+		Assert.assertTrue(list.get(2).equals("test3"));
+		Assert.assertTrue(list.get(3).equals("test4"));
+
+
+
+	}
+}
