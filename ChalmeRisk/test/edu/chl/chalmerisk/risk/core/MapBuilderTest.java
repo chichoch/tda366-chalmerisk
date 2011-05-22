d76c4394c4ed4d4a8d90ae7a9b5223acbeb5caf9
chichoch <chichoch@gmail.com>
2011-05-22 08:33:11 +0000

    More tests done.

---
+++ ChalmeRisk/test/edu/chl/chalmerisk/risk/core/MapBuilderTest.java
@@ -0,0 +1,25 @@
+package edu.chl.chalmerisk.risk.core;
+
+import java.io.FileNotFoundException;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class MapBuilderTest {
+	private MapBuilder map;
+	@Before
+	public void setUp() throws FileNotFoundException {
+		map = new MapBuilder("test/mapbuildertest.txt");
+	}
+	
+	@Test
+	public void TestMap() {
+		Assert.assertTrue(map.getIconFileName().equals("testfile"));
+		Assert.assertTrue(map.getContinents().size() == 2);
+		Assert.assertTrue(map.getCountries().size() == 4);
+		Assert.assertTrue(map.getCountry(1).getName().equals("test1"));
+	}
+}
